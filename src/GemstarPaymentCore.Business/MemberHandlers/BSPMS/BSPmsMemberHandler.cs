77a5eb8157465fbdd1c712d65b7338e4f25a9283
chenql <chenql163@163.com>
2019-08-20 11:17:04 +0800

    修复了调用捷云会员的时候，没有传递营业点的问题

--- src/GemstarPaymentCore.Business/MemberHandlers/BSPMS/BSPmsMemberHandler.cs
+++ src/GemstarPaymentCore.Business/MemberHandlers/BSPMS/BSPmsMemberHandler.cs
@@ -57,6 +57,7 @@ namespace GemstarPaymentCore.Business.MemberHandlers.BSPMS
                 .Append("<XType>JxdBSPms</XType>")
                 .Append("<OpType>会员交易</OpType>")
                 .Append("<ProfileCa>")
+                .Append("<OutletCode>").Append(para.OutletCode).Append("</OutletCode>")
                 .Append("<BalanceType>0102</BalanceType>")
                 .Append("<Amount>").Append(para.Amount > 0 ? -para.Amount : para.Amount).Append("</Amount>")
                 .Append("<Creator>jxdUnionPay</Creator>")
